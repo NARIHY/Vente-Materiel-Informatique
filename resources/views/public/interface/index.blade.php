2a9528c2cd14917957e281139163b6d2bd9e9354
NARIHY <maheninarandrianarisoa@gmail.com>
2023-09-26 09:19:18 +0200

    quelque modif sur l'interface

--- resources/views/public/interface/index.blade.php
+++ resources/views/public/interface/index.blade.php
@@ -120,33 +120,19 @@
         <div class="container">
             <h1 style="color:red;">Nos sponsors</h1>
 
-          <div class="row">
+            <div class="row">
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center mx-auto">
+                    <img src="{{asset('public/assets/img/clients/client-4.png')}}" class="img-fluid" alt="">
+                </div>
 
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="{{asset('public/assets/img/clients/client-1.png')}}" class="img-fluid" alt="">
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center mx-auto">
+                    <img src="{{asset('public/assets/img/clients/client-5.png')}}" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center mx-auto">
+                    <img src="{{asset('public/assets/img/clients/client-6.png')}}" class="img-fluid" alt="">
+                </div>
             </div>
-
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="{{asset('public/assets/img/clients/client-2.png')}}" class="img-fluid" alt="">
-            </div>
-
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="{{asset('public/assets/img/clients/client-3.png')}}" class="img-fluid" alt="">
-            </div>
-
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="{{asset('public/assets/img/clients/client-4.png')}}" class="img-fluid" alt="">
-            </div>
-
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="{{asset('public/assets/img/clients/client-5.png')}}" class="img-fluid" alt="">
-            </div>
-
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="{{asset('public/assets/img/clients/client-6.png')}}" class="img-fluid" alt="">
-            </div>
-
-          </div>
 
         </div>
       </section>
